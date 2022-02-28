2c9773085134bc40d5c5b9df45c09fd69fb361a9
Isabel Torres <isabel0729.it@gmail.com>
2022-02-28 11:04:34 +0100

    Componente Login-home, rutas, instalacion bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: HomeComponent },
+  // { path: 'calendar', component: CalendarComponent}
+  { path: '**', redirectTo: '/home' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  constructor(
+    private usersService: UsersService,
+    private router: Router
+  ) {
+
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required
+      ]),
+
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+    }, [])
+  }
+
+  ngOnInit(): void {
+  }
+
+  async onLogin(pForm: any) {
+    let response = await this.usersService.Login(pForm.value);
+    if (response.token) {
+      localStorage.setItem('token', response.token)
+      this.router.navigate(['home']);
+    } else {
+      alert(response.error)
+    }
+  }
+
+
+  checkControl(pcontrolName: string, errorName: string): boolean {
+    if (this.loginForm.get(pcontrolName)?.hasError(errorName) && this.loginForm.get(pcontrolName)?.touched) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
